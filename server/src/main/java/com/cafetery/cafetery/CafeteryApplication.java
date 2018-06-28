98b0b99bf7409223743dc78702f258e3db37d403
Yvgen Chemeris <chemeris.lunat1k@gmail.com>
2018-06-28 14:58:35 +0300

    server initial

---
+++ server/src/main/java/com/cafetery/cafetery/CafeteryApplication.java
@@ -0,0 +1,12 @@
+package com.cafetery.cafetery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteryApplication.class, args);
+	}
+}
